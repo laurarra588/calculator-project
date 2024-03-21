67c5b4292181c673d2fa2d0c973db56acbb1a5ba
Laura Silveira <laura.rra58@gmail.com>
2024-03-21 12:30:12 -0300

    Grid's buttons with QGridLayout in PySide6-Part 2

--- source/buttons.py
+++ source/buttons.py
@@ -1,5 +1,6 @@
 from PySide6.QtWidgets import QPushButton, QGridLayout, QWidget
 from variables import MEDIUM_FONT_SIZE
+from utils import isNumOrDot, isEmpty
 
 class Button(QPushButton):
     def __init__(self, *args, **kwargs):
@@ -15,12 +16,24 @@ class ButtonsGrid(QGridLayout):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
-        self._grid_mask = [
+        self._gridMask = [
             ['C', '◀', '^', '/'],
             ['7', '8', '9', '*'],
             ['4', '5', '6', '-'],
             ['1', '2', '3', '+'],
             ['',  '0', '.', '='],
         ]
+
+        self._makeGrid()
+
+    def _makeGrid(self):
+        for i , row in enumerate(self._gridMask):
+            for j , buttonText in enumerate(row):
+                button = Button(buttonText)
+
+                if not isNumOrDot(buttonText) and not isEmpty(buttonText):
+                    button.setProperty('cssClass', 'specialButton')
+
+                self.addWidget(button , i , j)
     
         
