259c1c0709edba7ee65ac306a46cd578b5e5c9ba
Laura Silveira <laura.rra58@gmail.com>
2024-03-21 09:27:28 -0300

    Creating a button with QPushButton in PySide6

--- source/main.py
+++ source/main.py
@@ -7,6 +7,7 @@ from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication
 from variables import WINDOW_ICON_PATH
 from styles import setupTheme
+from buttons import Button
 
 if __name__ == '__main__':
 
@@ -31,6 +32,10 @@ if __name__ == '__main__':
     # Display
     display = Display()
     window.addWidgetTovLayout(display)
+
+    # Button
+    button = Button('Texto do botão')
+    window.addWidgetTovLayout(button)
     
 
     window.adjustFixedSize()
